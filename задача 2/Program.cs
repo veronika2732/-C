ff2a4c26a58b521ebdc552765f186c6c72812ecc
Veronika <tokareva_v@inbox.ru>
2022-10-14 09:22:32 +0300

    first commit

---
+++ задача 2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Clear();
+Console.WriteLine("Введите первое число:");
+int x = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число:");
+int y = int.Parse(Console.ReadLine()!);
+if(x>y)
+{
+    Console.Write($"{x} больше!")
+}
+else
+{
+    Console.Write($"{Y}больше!")
+
+    }
+}
---
+++ задача 4/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+Console.WriteLine("Введите первое число");
+int x = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число");
+int y = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите третье число");
+int f = int.Parse(Console.ReadLine()!);
+if (x > y)
+{
+        Console.WriteLine("Максимальное число: " + x);
+    }
+    else
+    {
+        Console.WriteLine("Максимальное число: " + f);
+    }
+
+else if (y > f)
+{
+    Console.WriteLine("Максимальное число: " + y);
+}
+else
+{
+    Console.WriteLine("Максимальное число: " + f);
+}
+    
+    
